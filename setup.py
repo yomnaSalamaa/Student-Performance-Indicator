e4c5937c18ae0ce493416a6b43e77d694b9d4a34
Yomna Salama <Yomna.salama11@gmail.com>
2026-01-28 20:27:11 +0200

    Update setup.py and EDA notebook

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def get_requirements(file_path:str)->list[str]:
     return requirements
 
 setup(
-    name="mlproject",
+    name="Student Performance Prediction",
     version="0.0.1",
     author="Yomna Salama",
     author_email="yomna.salama11@gmail.com",
